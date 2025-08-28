a739ce872de234ecfa5fb67699e3a036cc9c2979
said-carrillo <L25550592@chihuahua2.tecnm.mx>
2025-08-27 21:46:21 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_5_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaracion de variables
+        //crearlas las variables para que se puedan usar
+        //tipo de datos identificador
+        String nombre;
+        byte edad;
+        //inicializacion
+        //usamos el operador =--> operador de asignacion
+        //pon "said ariel" dentro de la variable nombre
+        nombre = "said ariel";
+        edad = 20;
+        // uso de variable
+        System.out.println(nombre);
+        System.out.println(edad);
+        
+       
+    }
+    
+}
